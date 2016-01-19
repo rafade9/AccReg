9a3586bb63af7410be8577a3eb1208c6816730b4
rafade9 <rafade9@gmail.com>
2016-01-18 18:25:11 -0600

    Login Rest

--- src/com/gentera/cuentasn/controller/loginController.java
+++ src/com/gentera/cuentasn/controller/loginController.java
@@ -1,15 +1,24 @@
 package com.gentera.cuentasn.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gentera.cuentasn.entities.Usuario;
 
 @Controller
 public class loginController {
 
+	HttpServletRequest httpServlet;
+
 	/**
 	 * Funcion para atender recurso /welcome por GET
+	 * 
 	 * @param model
 	 * @return
 	 */
@@ -19,15 +28,48 @@ public class loginController {
 		model.addAttribute("message", 2);
 		System.out.print("hola");
 		return "loginYastas";
- 
+
 	}
-	
-	@RequestMapping(value = "/loginCompartamos", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String printLoginCompartamos(ModelMap model) {
 		System.out.println("Entra a welcome Compartamos");
-		model.addAttribute("message", 2);
-		return "loginYastas";
- 
+		return "loginCompartamos";
+
+	}
+
+	@RequestMapping(value = "/loginPrueba", method = RequestMethod.GET)
+	public String loginPrueba() {
+		System.out.println("Entra a login Prueba");
+		return "loginPrueba";
+	}
+
+	@RequestMapping(value = "/loginProcessCompartamos", method = RequestMethod.POST)
+	public ModelAndView loginProcessCompartamos(HttpServletRequest req, Usuario usuario) {
+		ModelAndView model;
+		try {
+			System.out.println(usuario.getUsername());
+			req.login(usuario.getUsername(), usuario.getPassword());
+			
+		} catch (ServletException e) {
+			
+			if(e.getMessage().startsWith("Bad credentials")){
+				model = new ModelAndView("loginPrueba");
+				model.addObject("error", "Usuario o password incorrecto");
+				return model;
+			}
+		}
+		model = new ModelAndView("index");
+		return model;
+	}
+
+	@RequestMapping(value = "/logoutCompartamos", method = RequestMethod.GET)
+	public void logoutCompartamos(HttpServletRequest req) {
+		try {
+			req.logout();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
 	}
 
 }

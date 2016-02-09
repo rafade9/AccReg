0274f5f2a4852ba0f21508b73c7a1ef6c20c72f4
marayazz <marayazz@gmail.com>
2016-02-09 17:55:47 -0600

    Se modifica controlador de generacion de pdf

--- src/com/gentera/cuentasn/controller/ComprobantePdfController.java
+++ src/com/gentera/cuentasn/controller/ComprobantePdfController.java
@@ -1,6 +1,5 @@
 package com.gentera.cuentasn.controller;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,43 +20,22 @@ public class ComprobantePdfController extends AbstractController{
 	@RequestMapping(value = "/imprimir", method = RequestMethod.GET)
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		
-		logger.info("*******Entre a imprimir el comprobante de cuenta N2*******");
-		
-		
-		Respuesta respuestaJSP = (Respuesta)request.getSession().getAttribute("respuesta");
-		
-		System.out.println("*****mi respuesta"+ respuestaJSP);
-		
-		Respuesta respuesta = new Respuesta();
-		respuesta.setIdOportunidad("123456");//folio
-		respuesta.setCLABE("234212123");
-		respuesta.setCuenta("08778876");
-		Persona persona = new Persona();
-		persona.setPrimerNombre("YOLANDA");
-		persona.setPaterno("OLMEDO");
-		persona.setMaterno("CANABAL");
-		persona.setFechaNacimiento("12/12/2000");
-		respuesta.setNumTarjeta("******2343");
-		
-		respuesta.setPersona(persona);
 
-//		if(output ==null || "".equals(output)){
-//			//return normal view
-//			return new ModelAndView("RevenueSummary","respuesta",respuesta);
-//
-//		}else if("PDF".equals(output.toUpperCase())){
-			//return excel view
+		logger.info("*******Entre a imprimir el comprobante de cuenta N2:*******");
+
+		Respuesta respuesta = (Respuesta)request.getSession().getAttribute("respuesta");
+
+		if(!respuesta.equals(null)){
+
+			Persona persona = new Persona();
+			persona = respuesta.getPersona();
+
+			respuesta.setPersona(persona);
+
 			return new ModelAndView("PdfRevenueSummary","respuesta",respuesta);
-//
-//		}else{
-//			//return normal view
-//			return new ModelAndView("comprobantepdf","respuesta",respuesta);
-//
-//		}
-
+		}else{
+			return new ModelAndView("RevenueSummary","respuesta",respuesta);
+		}
 	}
-	
-	
 
 }

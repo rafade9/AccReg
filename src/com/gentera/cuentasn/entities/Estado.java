cbc1fdbcf87c3d5efffb6c87aa69b7be52e94800
rafade9 <rafade9@gmail.com>
2016-02-18 12:04:49 -0600

    Ejemplo clase con comentarios

--- src/com/gentera/cuentasn/entities/Estado.java
+++ src/com/gentera/cuentasn/entities/Estado.java
@@ -15,26 +15,50 @@ public class Estado {
 	private String clave;
 	private String clavePais;
 
+	/**
+	 * Getter Nombre
+	 * @return the nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Setter Nombre
+	 * @param nombre the nombre to set
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Getter Clave
+	 * @return the clave
+	 */
 	public String getClave() {
 		return clave;
 	}
 
+	/**
+	 * Setter Clave
+	 * @param clave the clave to set
+	 */
 	public void setClave(String clave) {
 		this.clave = clave;
 	}
 
+	/**
+	 * Getter ClavePais
+	 * @return the clavePais
+	 */
 	public String getClavePais() {
 		return clavePais;
 	}
 
+	/**
+	 * Setter ClavePais
+	 * @param clavePais the clavePais to set
+	 */
 	public void setClavePais(String clavePais) {
 		this.clavePais = clavePais;
 	}

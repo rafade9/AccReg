93844910ec9477f260e3ad1bcff47a26e756799a
marayazz <marayazz@gmail.com>
2016-02-18 12:15:03 -0600

    Se modifica service para colocarse como tarea programada

--- src/com/gentera/cuentasn/service/impl/OperadoresYastasServiceImpl.java
+++ src/com/gentera/cuentasn/service/impl/OperadoresYastasServiceImpl.java
@@ -3,19 +3,23 @@ package com.gentera.cuentasn.service.impl;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Service;
 
 import com.gentera.cuentasn.service.OperadoresYastasService;
 import com.gentera.cuentasn.taks.ArchivoOperadoresTask;
+import com.gentera.cuentasn.util.OperadoresArchivo;
 
 @Service
-public class OperadoresYastasServiceImpl implements OperadoresYastasService{
+public class OperadoresYastasServiceImpl implements Job{
 
 	final static Logger logger = Logger.getLogger(OperadoresYastasServiceImpl.class);
 
 	@Override
-	public List<String> obtenerOperadoresY() {
-
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		
 		logger.info("Entrando a tarea programa de carga Operadores Yastas");
 		
 		List<String> operadores = ArchivoOperadoresTask.obtenerOperadores();
@@ -24,18 +28,18 @@ public class OperadoresYastasServiceImpl implements OperadoresYastasService{
 			
 			if(operadores.size()!=0){
 				
-				return operadores;
+				OperadoresArchivo.setNumOperadores(operadores);
+				
 			}else{
 				logger.error("**El archivo de Operadores esta vacio, no se puede establecer nueva lista de operadores**");
-				return null;
 			}
 		}
 		else{
 			logger.error("**El archivo de Operadores no existe, no se puede establecer nueva lista de operadores**");
-			return null;
+			
 		}
+		
 	}
-
 
 
 }

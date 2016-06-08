8c8dd26964327d068eabe07df088541b59b09bce
marayazz <marayazz@gmail.com>
2016-06-08 12:46:06 -0500

    se agrega boton de detalle y area de detalle

--- WebContent/resources/js/reposicionYastas.js
+++ WebContent/resources/js/reposicionYastas.js
@@ -147,7 +147,9 @@ $(document).ready(function(){
                 	}
                 	else{//mensaje generico
                 		$('#seccionError').css('display','block');
+                		$('#butonDetalle').css('display','block');
                 		$('#seccionError').html(mensajesRepo[data.codigo]);
+                		$('#detalle').append(data.mensaje);
                 	}
                 },
                 error: function(data){
@@ -189,7 +191,7 @@ $(document).ready(function(){
 			    function(value, element) {
 			        return value.match(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/);
 			    },
-			    "Por favor, capture una fecha v&aacute;lida.");
+			    "Capture una fecha v&aacute;lida.");
 
 
 //Valida que la fecha de nacimiento sea menor que la actual
@@ -318,7 +320,6 @@ $(document).ready(function(){
 					return true;
 				});
 		
-		
 });
 
 
@@ -342,3 +343,19 @@ $(function() {
 			
 	});
 });
+
+
+//Muestra detalle
+
+$(function() {
+	$("#btnDetalle").click(
+		function() {
+			if (detalle.style.display == "none") {
+					$('#detalle').css('display','block');
+			}else{
+				$('#detalle').css('display','none');
+			}
+	});
+});
+
+
